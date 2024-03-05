2feadb20e0cfbde9f486e2d999045d41fed7322e
rohan <rohansureshgupta@gmail.com>
2024-03-06 00:51:32 +0530

    added 6th March

---
+++ March_2024/Search_Pattern.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> search(string pattern, string text)
+    {
+        // code here.
+        vector<int> v;
+        int n = pattern.size();
+        for (int i = 0; i < text.size(); i++)
+        {
+            string s = text.substr(i, n);
+            if (pattern == s)
+            {
+                v.push_back(i + 1);
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector<int> res = ob.search(pat, S);
+        for (int i : res)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
