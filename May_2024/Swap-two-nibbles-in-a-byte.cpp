bbca4de5ce361d2e3dc80ed0f85ba7ca5f5fdf8f
rohan <rohansureshgupta@gmail.com>
2024-05-31 13:44:45 +0530

    31st May 2024

---
+++ May_2024/Swap-two-nibbles-in-a-byte.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int swapNibbles(int n)
+    {
+        // code here
+        int a = n;
+        int b = n;
+        return (((a & 15) << 4) | ((b & 240) >> 4));
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
