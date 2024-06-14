41c42c0bad3f5856d007b0bf2681aa74ffda9d03
rohan <rohansureshgupta@gmail.com>
2024-06-14 17:20:09 +0530

    14th June 2024

---
+++ June_2024/Armstrong-Numbers.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    string armstrongNumber(int number)
+    {
+        // code here
+        int num = 0;
+        int n = number;
+        while (n)
+        {
+            num += (pow(n % 10, 3));
+            n /= 10;
+        }
+        return num == number ? "true" : "false";
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
